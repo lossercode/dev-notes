a930a73f7f41e3454240c03327dc35f4812c197d
lossercode <2790372069@qq.com>
2026-06-15 09:58:44 +0800

    fix: 修复流水线报错问题

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+  basePath: "/dev-notes",
+  assetPrefix: "/dev-notes",
   images: {
     unoptimized: true,
   },
